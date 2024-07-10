0887d0f56918ae5f240d5d4557d7bb882e81bea1
kevin-mmoura <kevingameor12@gmail.com>
2024-07-10 16:14:25 -0300

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const CONFIG = require("./config.json")
 const myKey = ec.keyFromPrivate("36cde1004bc2435cb3c67b4de9d654f626e08077e6392781c866c80b7a0663af")
 const myWalletAdress = myKey.getPublic("hex")
 
-const tx1 = new Transaction(myWalletAdress, "Cleitin", 10)
+const tx1 = new Transaction(myWalletAdress, "Pedro", 10)
 tx1.signTransaction(myKey)
 
 const chain = new Blockchain(CONFIG.DIFFICULTY, CONFIG.MINING_REWARD)
@@ -16,5 +16,3 @@ const chain = new Blockchain(CONFIG.DIFFICULTY, CONFIG.MINING_REWARD)
 chain.addTransaction(tx1)
 chain.mineBlock(myWalletAdress)
 console.log(chain.getBalanceOfAdress(myWalletAdress))
-chain.chain[1].transactions[0].amount = 80
-console.log(`É válido?  ${chain.isValid()}`)
